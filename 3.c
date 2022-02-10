434c99eab274316c0ff73bd81b4ae47ef0c0e87a
Hajar Khouz <96071649+hajarkhouz@users.noreply.github.com>
2022-02-10 13:33:07 +0100

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+int x;
+float y;
+	printf("Saisir la valeur de x : ");
+	scanf( "%d", &x);
+		printf("Saisir la valeur de y : ");
+	scanf( "%f", &y);
+	printf("x =%d et y=%f", x,y);	
+
+	return 0;
+	
+}
